f9861a6158da92be3e893b8dc1c6db2b2f6d5c0c
Luiz Bukowski <emaildotaborda@gmail.com>
2025-08-04 10:06:56 -0300

    Logs da execução

--- run.sh
+++ run.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
-echo #Setando variáveis
+echo "#Setando variáveis"
 URL=https://www.tesourodireto.com.br/json/br/com/b3/tesourodireto/service/api/treasurybondsinfo.json
 FILE_PATH=data/tesouro_direto.json
 
-echo #Atualizando repositório
+echo "#Atualizando repositório"
 git pull
 
-echo #Atualizando $FILE_PATH com $URL
+echo "#Atualizando $FILE_PATH com $URL"
 curl $URL | jq > $FILE_PATH
 
-echo #Comitando atualização
+echo "#Enviando atualização"
 git add $FILE_PATH
 git commit -m "Atualização Tesouro Direto"
 git push
 
-echo  # Finishi #
+echo  "## Finishi ##"
