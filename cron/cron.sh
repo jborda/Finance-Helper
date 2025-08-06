f8cb5ef486ed3588940873e06d7f8713aca8c11e
Luiz Bukowski <emaildotaborda@gmail.com>
2025-08-06 09:52:34 -0300

    Cron, ajustes

--- cron/cron.sh
+++ cron/cron.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 echo "#Configura o cron para atualizar os valores do Tesouro Direto diariamente"
-RUN_PATH=/home/jborda/Dropbox/Desenvolvimento/github/Finance-Helper
+PROJECT_PATH=/home/jborda/Dropbox/Desenvolvimento/github/Finance-Helper
 
 echo #Criando estrutura#
-chmod +x $RUN_PATH/run.sh
+chmod +x $PROJECT_PATH/run.sh
 
 echo "#Configurando cron"
-crontab cron.cfg
+crontab $PROJECT_PATH/cron/cron.cfg
 crontab -l
 
 echo "## FINISH ##"
--- run.sh
+++ run.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 echo "#Setando variáveis"
 URL=https://www.tesourodireto.com.br/json/br/com/b3/tesourodireto/service/api/treasurybondsinfo.json
+PROJECT_PATH=/home/jborda/Dropbox/Desenvolvimento/github/Finance-Helper
 FILE_PATH=data/tesouro_direto.json
 
 echo "#Atualizando repositório"
-git pull
+git -C $PROJECT_PATH pull
 
 echo "#Atualizando $FILE_PATH com $URL"
 curl $URL | jq > $FILE_PATH
 
 echo "#Enviando atualização"
-git add $FILE_PATH
-git commit -m "Atualização Tesouro Direto"
-git push
+git -C $PROJECT_PATH add $FILE_PATH
+git -C $PROJECT_PATH commit -m "Atualização Tesouro Direto"
+git -C $PROJECT_PATH push
 
 echo  "## Finishi ##"
